d926dbd181afbc93a062ce3bb70f3eecb4558b47
cesar-guillen <guillencesar871@gmail.com>
2025-08-29 13:16:23 +0200

    more functional, correct decryptio, missing content table and github workflow integration

--- _plugins/protector.rb
+++ _plugins/protector.rb
@@ -1,6 +1,7 @@
 require 'base64'
 require 'digest'
 require 'openssl'
+require 'nokogiri'
 
 def aes256_encrypt(password, cleardata)
   digest = Digest::SHA256.new
@@ -20,58 +21,58 @@ def aes256_encrypt(password, cleardata)
   "#{encoded_iv}|#{hmac}|#{encoded_msg}"
 end
 
-Dir.glob('_site/posts/*/index.html').each do |post_path|
-  html = File.read(post_path)
-  password = ENV['PROTECTOR_PASSWORD'] || 'changeme' # <--- set your password here
-  encrypted = aes256_encrypt(password, html)
+require 'nokogiri'
+require 'json'
 
-  protected_html = <<~HTML
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Protected Post</title>
-        <meta charset="utf-8">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"></script>
-      </head>
-      <body>
-        <div id="content">
-          <input id="password" type="password" placeholder="Enter password">
-          <button onclick="decrypt()">Decrypt</button>
-          <p id="errmsg" style="color: red;"></p>
-        </div>
-        <script>
-          function decrypt() {
-            var protectedContent = "#{encrypted}";
-            var password = document.getElementById('password').value;
-            var payload = protectedContent.split("|");
-            var iv = payload[0];
-            var hmac = payload[1];
-            var cipherText = payload[2];
-            var passphraseDgst = CryptoJS.SHA256(password).toString();
-            var decryptedhmac = CryptoJS.HmacSHA256(cipherText, CryptoJS.enc.Hex.parse(passphraseDgst)).toString().trim();
-            if(CryptoJS.enc.Base64.parse(hmac).toString() === decryptedhmac){
-              var decrypted = CryptoJS.AES.decrypt(
-                {ciphertext:CryptoJS.enc.Base64.parse(cipherText)},
-                CryptoJS.enc.Hex.parse(passphraseDgst),
-                {iv:CryptoJS.enc.Base64.parse(iv)}
-              );
-              var content = CryptoJS.enc.Utf8.stringify(decrypted);
-              document.getElementById('content').innerHTML = content;
-            } else {
-              document.getElementById('errmsg').innerHTML = "Wrong password";
-            }
+Dir.glob('_site/posts/*/index.html').each do |post_path|
+  password = ENV['PROTECTOR_PASSWORD'] || 'changeme'
+
+  html = File.read(post_path)
+  doc = Nokogiri::HTML(html)
+
+  content_node = doc.at_css('div.content')
+  next unless content_node
+
+  content_to_encrypt = content_node.inner_html
+  encrypted = aes256_encrypt(password, content_to_encrypt)
+  encrypted_js = encrypted.to_json  # safe for JS
+
+  protected_block = <<~HTML
+    <div class="content">
+      <div id="protected">
+        Insert password for decryption<br>
+        <input id="password" type="password">
+        <button onclick="decrypt()">Decrypt</button>
+        <p id="errmsg" style="color: red;"></p>
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js"></script>
+      <script>
+        function decrypt() {
+          var protectedContent = #{encrypted_js};
+          var password = document.getElementById('password').value;
+          var payload = protectedContent.split("|");
+          var iv = payload[0], hmac = payload[1], cipherText = payload[2];
+          var passphraseDgst = CryptoJS.SHA256(password).toString();
+          var decryptedhmac = CryptoJS.HmacSHA256(cipherText, CryptoJS.enc.Hex.parse(passphraseDgst)).toString().trim();
+
+          if (decryptedhmac === CryptoJS.enc.Base64.parse(hmac).toString()) {
+            var decrypted = CryptoJS.AES.decrypt(
+              {ciphertext: CryptoJS.enc.Base64.parse(cipherText)},
+              CryptoJS.enc.Hex.parse(passphraseDgst),
+              {iv: CryptoJS.enc.Base64.parse(iv)}
+            );
+            var content = CryptoJS.enc.Utf8.stringify(decrypted);
+            document.getElementById('protected').innerHTML = content;
+          } else {
+            document.getElementById('errmsg').innerText = "Wrong password";
           }
-          var passwordInput = document.getElementById('password');
-          passwordInput.addEventListener("keyup", function(event) {
-            event.preventDefault();
-            if (event.keyCode === 13) {
-              decrypt();
-            }
-          });
-        </script>
-      </body>
-    </html>
+        }
+      </script>
+    </div>
   HTML
 
-  File.write(post_path, protected_html)
+  fragment = Nokogiri::HTML::DocumentFragment.parse(protected_block)
+  content_node.replace(fragment)
+
+  File.write(post_path, doc.to_html)
 end
